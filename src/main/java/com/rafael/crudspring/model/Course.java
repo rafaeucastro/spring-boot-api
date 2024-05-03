fdc39b52503efaa8259b7661a385c735081471d9
Rafael Castro <rafael.scastro@dataprev.gov.br>
2024-05-03 11:47:12 -0300

    Remoção Lógica

--- src/main/java/com/rafael/crudspring/model/Course.java
+++ src/main/java/com/rafael/crudspring/model/Course.java
@@ -1,5 +1,7 @@
 package com.rafael.crudspring.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.persistence.Column;
@@ -16,6 +18,8 @@ import lombok.Data;
 @Entity
 // Descomentar a linha abaixo caso já exista uma tabela Cursos criada
 // @Table(name = "Cursos")
+@SQLDelete(sql="UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
     @Id
@@ -33,5 +37,8 @@ public class Course {
     @Column(length=10, nullable = false)
     private String category;
 
-    
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length=10, nullable = false)
+    private String status="Ativo";
 }
