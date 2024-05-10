6ff5f246c770caf263478e77951a0830f6075d55
Rafael Castro <rafael.scastro@dataprev.gov.br>
2024-05-10 11:02:12 -0300

    Não trazer o atributo curso em Lesson

--- src/main/java/com/rafael/crudspring/model/Lesson.java
+++ src/main/java/com/rafael/crudspring/model/Lesson.java
@@ -1,5 +1,6 @@
 package com.rafael.crudspring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -25,5 +26,6 @@ public class Lesson {
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id", nullable = false)
+    @JsonProperty(access=JsonProperty.Access.WRITE_ONLY)
     private Course course;
 }
